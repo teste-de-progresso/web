532940e1df35c28d69ba403aa281b92a4f7b6108
Claudio Ramos <mello.lcj@gmail.com>
2020-07-21 20:10:41 -0300

    card grid

--- src/components/layout/Page.jsx
+++ src/components/layout/Page.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import {MyIp} from "../myip/myip";
 import {Counter} from "../counter/Counter";
+import {CardGrid} from "../widgets/CardGrid";
 
 export const Page = () => {
     return (
         <div className="bg-primary-normal w-full h-full">
             <main className="bg-gray-100 py-4 px-8 rounded-t-xlg h-full">
-                <div className="flex flex-row">
+                <CardGrid>
                     <Counter/>
                     <MyIp/>
-                </div>
+                    <MyIp/>
+                </CardGrid>
             </main>
         </div>
     )
---
+++ src/components/widgets/CardGrid.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Grid = styled.div`
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+`
+
+export const CardGrid = ({children}) => {
+
+    return (
+        <Grid>
+            {children}
+        </Grid>
+    )
+}
