931897ba23b5f4ed08d37c8bbb4ce1334555ea44
João Geonizeli <joao.geonizeli@gmail.com>
2020-08-02 23:07:56 -0300

    add react-hook-form and use on ckeditor

--- src/components/screens/questions/Form/Form.jsx
+++ src/components/screens/questions/Form/Form.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextEditor } from "./TextEditor";
+import { Button } from "../../../widgets/Button";
+import { useForm } from "react-hook-form";
 
 export const Form = (props) => {
+  const { register, handleSubmit } = useForm();
+
+  const [ckData, setCkData] = useState();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="bg-gray-100 py-4 px-8">
-      <TextEditor data={props.data} />
-    </div>
+    <form className="bg-gray-100 py-4 px-8" onSubmit={handleSubmit(onSubmit)}>
+      <input name="body" ref={register} value={ckData} hidden={true} />
+      <TextEditor data={props.data} setCkData={setCkData} />
+
+      <Button>Salvar</Button>
+    </form>
   );
 };
