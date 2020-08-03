9f7ac33ffa036edef8c22c72ab902957796c5c8f
João Geonizeli <joao.geonizeli@gmail.com>
2020-08-02 23:41:40 -0300

    create question alternative field

--- src/components/layout/Page.jsx
+++ src/components/layout/Page.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import { Form } from "../screens/questions/Form";
 
 export const Page = () => {
+
+  const questionData = {
+    body: "<p>fon</p>"
+  }
+
   return (
-    <div className="bg-primary-normal w-full h-full">
-      <main className="bg-gray-100 py-4 px-8 rounded-t-xlg h-full">
-        <Form data="" />
+    <div className="bg-gray-100 py-4 px-8 rounded-t-xlg h-full w-full">
+      <main className="max-w-2xl">
+        <Form questionData={questionData} />
       </main>
     </div>
   );
