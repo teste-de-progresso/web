522396ef661f3a148ee1e70cc5206f9574406301
Claudio Ramos <mello.lcj@gmail.com>
2020-08-05 19:24:21 -0300

    ui improvements

--- src/components/layout/Page.jsx
+++ src/components/layout/Page.jsx
@@ -3,15 +3,15 @@ import { Form } from "../screens/questions/form";
 
 export const Page = () => {
 
-  const questionData = {
-    body: "<p>fon</p>"
-  }
+    const questionData = {
+        body: "<p>fon</p>"
+    }
 
-  return (
-    <div className="flex justify-center bg-gray-100 py-4 px-8 rounded-t-xlg h-full w-full">
-      <main className="min-w-full max-w-6xl">
-        <Form questionData={questionData} />
-      </main>
-    </div>
-  );
+    return (
+        <div className="bg-primary-normal h-full w-full">
+            <main className="bg-gray-100 py-4 px-8 rounded-t-xlg h-full">
+                <Form questionData={questionData} />
+            </main>
+        </div>
+    );
 };
