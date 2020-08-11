3720bd27f678b4d534992341c9e3c404455f3111
ricardo <dinhofonte@hotmail.com>
2020-08-10 22:25:13 -0300

    Finished featureForm view

--- src/components/screens/questions/form/FeaturesForm.jsx
+++ src/components/screens/questions/form/FeaturesForm.jsx
@@ -2,53 +2,54 @@ import React from "react";
 import {Card} from "../../../widgets/Card";
 
 export const FeaturesForm = () => {
+    const currentYear = new Date().getFullYear()
+
     return (
         <Card title={"Características"}>
             <div className="flex flex-col space-y-4">
                 <div className={"flex space-x-2"}>
                     <div className="w-full">
-                        <h4>Autoria</h4>
+                        <h2>Autoria</h2>
                         <select className="w-full bg-gray-100 rounded p-2 shadow-sm border border-gray-200">
                             <option selected={true}>Própria</option>
                             <option>Terceiros</option>
                         </select>
                     </div>
                     <div className="w-full">
-                        <h4>Autoria</h4>
+                        <h2>Tipo</h2>
                         <select className="w-full bg-gray-100 rounded p-2 shadow-sm border border-gray-200">
-
+                            <option>Objetiva</option>
+                            <option>Discurssiva</option>
                         </select>
                     </div>
                 </div>
                 <div className={"border bg-white border-gray-300 rounded shadow-sm"}>
                     <div className="p-4 flex space-x-2">
                         <div className="w-full">
-                            <h4>Referência</h4>
-                            <select className="w-full bg-gray-100 rounded p-2 shadow-sm border border-gray-200">
-                                <option selected={true}>Própria</option>
-                                <option>Terceiros</option>
-                            </select>
+                            <h2>Referência</h2>
+                            <textarea className="w-full rounded shadow-sm border border-gray-200"> </textarea>
                         </div>
-                        <div className="w-full">
-                            <h4>Ano</h4>
-                            <select className="w-full bg-gray-100 rounded p-2 shadow-sm border border-gray-200">
-
-                            </select>
+                        <div className="w-full  ">
+                            <h2>Ano</h2>
+                            <input className="w-full bg-gray-100 rounded p-2 shadow-sm border border-gray-200" type="number" min="1999" max={currentYear} step="1" />
                         </div>
                     </div>
                 </div>
                 <div className={"flex space-x-2"}>
                     <div className="w-full">
-                        <h4>Taxonomia de Bloom</h4>
+                        <h2>Taxonomia de Bloom</h2>
                         <select className="w-full bg-gray-100 rounded p-2 shadow-sm border border-gray-200">
-                            <option selected={true}>Própria</option>
-                            <option>Terceiros</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
                         </select>
                     </div>
                     <div className="w-full">
-                        <h4>Dificuldade</h4>
+                        <h2>Dificuldade</h2>
                         <select className="w-full bg-gray-100 rounded p-2 shadow-sm border border-gray-200">
-
+                        <option>Fácil</option>
+                        <option>Moderada</option>
+                        <option>Difícil</option>
                         </select>
                     </div>
                 </div>
