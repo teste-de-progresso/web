38319547d5ba001ee12b9748b9e35146479de1c3
João Geonizeli <joao.geonizeli@gmail.com>
2020-08-17 21:35:30 -0300

    config backend as envirioment variable

--- src/components/screens/questions/form/TextEditor.jsx
+++ src/components/screens/questions/form/TextEditor.jsx
@@ -1,8 +1,8 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import CKEditor from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import {Controller} from "react-hook-form";
-import {FormContext} from "../../../layout/SteppedForm";
+import { Controller } from "react-hook-form";
+import { FormContext } from "../../../layout/SteppedForm";
 
 const toolbarOptions = [
   "bold",
@@ -20,25 +20,27 @@ const toolbarOptions = [
   "redo",
 ];
 
-export const TextEditor = ({name, defaultValue = ""}) => {
+export const TextEditor = ({ name, defaultValue = "" }) => {
   const formContext = useContext(FormContext);
 
-  return <Controller
+  return (
+    <Controller
       control={formContext.control}
       name={name}
       defaultValue={defaultValue}
-      render={({onChange, value}) => (
-          <CKEditor
-              editor={ClassicEditor}
-              data={value}
-              config={{
-                toolbar: toolbarOptions,
-                ckfinder: {
-                  uploadUrl:
-                      "https://progress-test-backend.herokuapp.com/picture/upload",
-                },
-              }}
-              onChange={(_, editor) => onChange(editor.getData())}
-          />
-      )}/>
+      render={({ onChange, value }) => (
+        <CKEditor
+          editor={ClassicEditor}
+          data={value}
+          config={{
+            toolbar: toolbarOptions,
+            ckfinder: {
+              uploadUrl: process.env.REACT_APP_BACKEND_URL + "/picture/upload",
+            },
+          }}
+          onChange={(_, editor) => onChange(editor.getData())}
+        />
+      )}
+    />
+  );
 };
