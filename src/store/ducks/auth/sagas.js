6f78f88103abd56548b05c3910ebf4b406941bb9
João Geonizeli <joao.geonizeli@gmail.com>
2020-09-01 21:17:13 -0300

    add env var for jwt

--- src/store/ducks/auth/sagas.js
+++ src/store/ducks/auth/sagas.js
@@ -1,14 +1,14 @@
-import {failedLoadAuthentication, failureAuthentication, successAuthentication} from "./actions";
-import {put} from "@redux-saga/core/effects";
+import { failedLoadAuthentication, failureAuthentication, successAuthentication } from "./actions";
+import { put } from "@redux-saga/core/effects";
 import AuthenticationService from "../../../services/AuthenticationService";
 import * as jwt from "jsonwebtoken";
 
 export function* authenticationRequested(action) {
-    const {email, password} = action.payload;
+    const { email, password } = action.payload;
 
     const [result, data] = yield AuthenticationService.login(email, password);
 
-    if(result) {
+    if (result) {
         yield put(successAuthentication(data));
         localStorage.setItem("auth", data);
     } else {
@@ -19,8 +19,9 @@ export function* authenticationRequested(action) {
 
 export function* authentcationLoad() {
     const token = localStorage.getItem("auth");
+    const guid = process.env.REACT_APP_JWT_SECRET_KEY || '1cb26f40-498b-4f72-a00a-e8633abc5957'
 
-    if(token && jwt.verify(token, '1cb26f40-498b-4f72-a00a-e8633abc5957')) {
+    if (token && jwt.verify(token, guid)) {
         yield put(successAuthentication(token));
     } else {
         yield put(failedLoadAuthentication());
