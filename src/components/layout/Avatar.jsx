5abb0fc4a3db3301ce687d8c03675625e3ab9468
João Geonizeli <joao.geonizeli@gmail.com>
2020-09-13 16:34:39 -0300

    resolve merge conflicts

--- src/components/layout/Avatar.jsx
+++ src/components/layout/Avatar.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 
-export const Avatar = ({src}) => {
-
+export const Avatar = ({ src, className }) => {
   const url = () => {
-    if (!src) return "https://via.placeholder.com/150"
+    if (!src) return "https://via.placeholder.com/150";
 
-    return process.env.REACT_APP_BACKEND_URL + src
-  }
+    return process.env.REACT_APP_BACKEND_URL + src;
+  };
 
   return (
     <img
-      className="rounded-full w-12 border-2 border-primary-light shadow"
+      className={`rounded-full border-2 border-primary-light shadow ${className}`}
       src={url()}
       alt="Avatar do usuário"
-      style={style}
     />
   );
 };
--- src/components/screens/user_profile/UserProfile.jsx
+++ src/components/screens/user_profile/UserProfile.jsx
@@ -1,29 +1,27 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import { Button } from "../../widgets/Button";
-import {Avatar} from "../../layout/Avatar";
+import { useUserContext } from "../../utils";
+import { Avatar } from "../../layout/Avatar";
 
 export const UserProfile = () => {
-  const history = useHistory();
+  const userContextData = useUserContext();
+  const userInfo = userContextData?.userInfo;
 
-
-    return (
-        <div className="bg-primary-normal h-full w-full">
-            <main className="bg-gray-100 py-4 px-8 rounded-t-xlg h-full">
-                <div className="flex items-center flex-col">
-                    <div className="bg-white shadow border border-gray-100 flex flex-col items-center rounded p-4 w-full max-w-4xl pb-4 mt-8">
-                        <div className="relative w-full flex justify-center h-16">
-                            <Avatar className="top-0 absolute" size={24} style={{top: '-3.25rem'}}/>
-                        </div>
-                        <h2 className="font-xl font-bold">Eugênio da Silva </h2>
-                        <h2 className="font-xl py-4">Centro de Ciências Tecnológicas (CCT)</h2>
-                        <h2 className="font-xl">Docente</h2>
-                    </div>
-                    <div className="pt-2 flex justify-end max-w-4xl w-full">
-                        <Button className="">Editar dados</Button>
-                    </div>
-                </div>
-            </main>
+  return (
+    <div className="bg-primary-normal h-full w-full">
+      <main className="bg-gray-100 py-4 px-8 rounded-t-xlg h-full">
+        <div className="flex items-center flex-col max-w-4xl m-auto">
+          <div className="bg-white shadow border border-gray-100 flex flex-col items-center rounded p-4 w-full mt-12 mb-4 relative">
+            <div className="w-20 absolute" style={{ top: "-3.10rem" }}>
+              <Avatar src={userInfo.avatarUrl} />
+            </div>
+            <div className="mt-8 text-center">
+              <h2 className="font-bold">{userInfo.name}</h2>
+              <h2 className="py-4">TODO: Centro</h2>
+              <h2 className="">TODO: Cargo</h2>
+            </div>
+          </div>
         </div>
-    );
+      </main>
+    </div>
+  );
 };
