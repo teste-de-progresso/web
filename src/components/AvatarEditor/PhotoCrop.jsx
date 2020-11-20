ea84592b893606f70de14b80db862a5feadd392d
João Geonizeli <joao.geonizeli@gmail.com>
2020-11-19 22:09:47 -0300

    refactor photocrop

--- src/components/AvatarEditor/PhotoCrop.jsx
+++ src/components/AvatarEditor/PhotoCrop.jsx
@@ -1,58 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "react-avatar-edit";
 
-export class PhotoCrop extends React.Component {
-  constructor(props) {
-    super(props);
-    const src = "";
-    this.state = {
-      preview: null,
-      src,
-    };
+export const PhotoCrop = ({ callback }) => {
+  const [result, setResult] = useState();
 
-    this.onCrop = this.onCrop.bind(this);
-    this.onClose = this.onClose.bind(this);
-    this.onBeforeFileLoad = this.onBeforeFileLoad.bind(this);
+  const onCrop = (cropped) => {
+    setResult(cropped)
+    callback(result)
   }
 
-  onClose() {
-    this.setState({ preview: null });
+  const onClose = () => {
+    setResult(null)
   }
 
-  onCrop(preview) {
-    this.setState({ preview });
-    this.props.callback(preview);
-  }
-
-  onBeforeFileLoad(elem) {
-    if (elem.target.files[0].size > 120000) {
+  const onBeforeFileLoad = (elem) => {
+    if (elem.target.files[0].size > 180000) {
       alert("File is too big!");
       elem.target.value = "";
     }
   }
 
-  render() {
-    const sizes = 300;
-    return (
-      <div className="flex flex-row justify-between p-2 max-w-screen-sm m-auto">
-        <Avatar
-          width={sizes}
-          height={sizes}
-          imageWidth={sizes}
-          imageHeight={sizes}
-          onCrop={(e) => this.onCrop(e)}
-          onClose={this.onClose}
-          onBeforeFileLoad={this.onBeforeFileLoad}
-          src={this.state.src}
-        />
-        <img
-          width={sizes}
-          height={sizes}
-          src={this.state.preview}
-          alt="Preview"
-          className="hidden"
-        />
-      </div>
-    );
-  }
+  const dimention = 300;
+
+  return (
+    <Avatar
+      width={dimention}
+      height={dimention}
+      imageWidth={dimention}
+      imageHeight={dimention}
+      onCrop={(e) => onCrop(e)}
+      onClose={() => onClose()}
+      onBeforeFileLoad={onBeforeFileLoad}
+    />
+  );
 }
